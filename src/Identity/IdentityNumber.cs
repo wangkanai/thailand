4a737aab8395d6f5e8d5332846d0a6ef75d0a99b
wangkanai <sarin@wangkanai.com>
2025-03-17 10:30:32 +0700

    Refactor identity validation logic and add Wangkanai.System.
    Renamed `IdentifierNumber` and related classes to `IdentityNumber`. Implemented the `Validate` method for validating Thai identity numbers. Added `Wangkanai.System` package to project dependencies and updated documentation accordingly.

--- src/Identity/IdentityNumber.cs
+++ src/Identity/IdentityNumber.cs
@@ -1,6 +1,6 @@
 namespace Wangkanai.Thailand.Identity;
 
-public sealed class IdentifierNumber
+public sealed class IdentityNumber
 {
     public ulong Number { get; private set; }
     public bool Valid { get; private set; }
