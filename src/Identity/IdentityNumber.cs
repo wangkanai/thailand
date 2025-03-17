b59f47a41f8282fa0481930aad39e623fc3f850e
wangkanai <sarin@wangkanai.com>
2025-03-17 11:16:59 +0700

    Refactor identity number validation logic.
    Moved validation logic from a separate static class to the `IdentityNumber` class for improved cohesion. Updated method signature and tests to accommodate this change, ensuring validation for numbers with exactly 13 digits only.

--- src/Identity/IdentityNumber.cs
+++ src/Identity/IdentityNumber.cs
@@ -3,8 +3,29 @@ namespace Wangkanai.Thailand.Identity;
 public sealed class IdentityNumber
 {
 	public ulong Number { get; private set; }
-	public bool  Valid  { get; private set; }
+	public bool  IsValid  { get; private set; }
 
 	public IdentityNumber() { }
 	public IdentityNumber(ulong number) => Number = number;
+
+	public static bool Validate(ulong number)
+	{
+		if(number is > 9_999_999_999_999 or < 0_200_000_000_000)
+			return false;
+
+		var digits = new int[13];
+
+		ReadOnlySpan<char> span = number.ToString();
+		for (var i = 0; i < span.Length; i++)
+			digits[i] = span[i] - '0';
+
+		var sum = 0;
+		for (var i = 0; i < 13; i++)
+			sum += digits[i] * (i + 1);
+
+		var mod        = sum % 11;
+		var checkDigit = mod <= 1 ? 1 - mod : 11 - mod;
+
+		return digits[12] == checkDigit;
+	}
 }
--- tests/unit/Identity/IdentityNumberValidationTests.cs
+++ tests/unit/Identity/IdentityNumberValidationTests.cs
@@ -1,15 +1,22 @@
 // Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
-using Wangkanai.Thailand.Identity.Identifier;
+using Wangkanai.Thailand.Identity;
 
 namespace Wangkanai.Thailand.Identity;
 
 public class IdentityNumberValidationTests
 {
 	[Fact]
-	public void Validate_ValidIdentityNumber_ReturnsTrue()
+	public void MoreThen13digits_ReturnsFalse()
 	{
-		var id = new IdentityNumber(1234567890123);
-		Assert.True(id.Validate());
+		ulong number = 12345678901234567890;
+		Assert.False(IdentityNumber.Validate(number));
+	}
+
+	[Fact]
+	public void LessThen13digits_ReturnsFalse()
+	{
+		ulong number = 0_123_456_789_012;
+		Assert.False(IdentityNumber.Validate(number));
 	}
 }
