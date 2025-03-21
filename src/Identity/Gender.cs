0665f92bff3df496d805cce64b8ab26439c763d2
wangkanai <sarin@wangkanai.com>
2025-03-21 11:40:09 +0700

    Add Gender enum to represent male and female values
    Introduced a `Gender` enum in the `Wangkanai.Thailand.Identity` namespace with `Male` and `Female` options, including description attributes in Thai. This provides a standardized way to handle gender representation in the application.

---
+++ src/Identity/Gender.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using System.ComponentModel;
+
+namespace Wangkanai.Thailand.Identity;
+
+public enum Gender
+{
+	[Description("ชาย")]
+	Male,
+	[Description("หญิง")]
+	Female
+}
