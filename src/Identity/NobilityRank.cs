723283e55984c99e31b14543d5afb16802a0b1c9
wangkanai <sarin@wangkanai.com>
2025-03-27 11:12:09 +0700

    Add NobilityRank class to Identity namespace
    Introduce `NobilityRank` as a sealed class inheriting from `Rank`, with constructors for Thai and English titles and acronyms. This addition provides better handling of nobility rank metadata in the identity domain.

---
+++ src/Identity/NobilityRank.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Thailand.Identity;
+
+public sealed class NobilityRank : Rank
+{
+	public NobilityRank() { }
+
+	public NobilityRank(string titleThai, string titleEnglish, string acronymThai, string acronymEnglish)
+	{
+		TitleThai      = titleThai;
+		TitleEnglish   = titleEnglish;
+		AcronymThai    = acronymThai;
+		AcronymEnglish = acronymEnglish;
+	}
+}
